b291ad7a14ebccaaed0457914a6f7e4b71c9bb4c
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2023-01-28 08:34:50 -0500

    update some deps

--- src/Login.cy.tsx
+++ src/Login.cy.tsx
@@ -1,7 +1,7 @@
 import { LoginBtn, Location } from './Login'
 
 describe('Login button', { viewportWidth: 300, viewportHeight: 300 }, () => {
-  it.only('tries to click the button', () => {
+  it.skip('tries to click the button', () => {
     cy.mount(<LoginBtn />)
     cy.getByCy('login-button').click()
     // cannot really access the new domain from the component test
@@ -9,9 +9,7 @@ describe('Login button', { viewportWidth: 300, viewportHeight: 300 }, () => {
 
   it('stubs location.assign via its proxy object', () => {
     cy.stub(Location, 'assign').as('assign')
-
     cy.mount(<LoginBtn />)
-
     cy.getByCy('login-button').click()
     cy.get('@assign').should('have.been.calledOnceWith', 'https://cypress.tips')
   })
